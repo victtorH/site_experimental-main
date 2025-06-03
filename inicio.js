42696dd1aa818a5661107c3589ffedf485bf2374
victtorH <estevamvicttor@gmail.com>
2025-06-03 16:26:22 -0300

    skills

--- inicio.js
+++ inicio.js
@@ -13,12 +13,17 @@ var esp3 = document.querySelector('.esp3')
 var esp4 = document.querySelector('.esp4')
 var topo = document.querySelectorAll('[data-topo]')
 var linear = document.querySelector('.linear')
+var portas = document.querySelectorAll('[data-porta]')
 
 
 
 window.addEventListener('load',()=>{
 
      linear.classList.add('baixo')
+     portas.forEach(porta =>{
+      porta.classList.add('baixoporta')
+     })
+     
   
 })
 
@@ -30,10 +35,28 @@ topo.forEach( Element => {
 if(windowof > Element.offsetTop +385 ){
 
      Element.classList.remove('baixo')
+      ul.style = ('box-shadow: 1px 1px 10px 0.5px rgba(0, 0, 0, 0.18);margin-top:2em;')
+     
 }else if(windowof > Element.offsetTop -200 ) {
   
       Element.classList.add('baixo')
+    
+      ul.style = (' background-color: transparent; box-shadow:none;margin-top:0px;')
+}})})
 
+ window.addEventListener('scroll', ( ) => {
+ const windowof = window.pageYOffset
+portas.forEach( Element => {   
+    
+if(windowof > Element.offsetTop -150 ){
+
+      Element.classList.remove('baixoporta')
+
+}else{
+  
+      Element.classList.add('baixoporta')
+
+     
 }})})
 
 var anime = document.querySelectorAll('[data-anime]')
@@ -103,17 +126,74 @@ esp2.style = ('transition: all 3s ease-in-out;')
 esp3.style = ('transition: all 1s ease-in-out;')
 esp4.style = ('transition: all 0.7s ease-in-out;')
 
-      conta++
+      contador--
     }
 
   }
+
+  
+let contaimg = 0
+
+
+var topics = document.querySelectorAll('.topic')
+
+function mostrarimg(){
+  let imgativa = document.querySelector('.img.ativo');
+  var img = document.querySelectorAll('.img')
+
+    let topicativo = document.querySelector('.sessao .topic.ativo')
+    
+
+
+
+  if(imgativa == img[contaimg]){
+
+  if(contaimg < 3 ){
+    contaimg++
+     
+  }else{
+    contaimg = 0
+  }
+  }else{
+    console.log(contaimg)
+   topics[contaimg].classList.add('ativo')
+    topicativo.classList.remove('ativo')
+
+  imgativa = imgativa.classList.remove('ativo')
+  setTimeout(()=>{
+img = img[contaimg].classList.add('ativo')
+  },500)
+
+}
+    
+}
+
+setInterval(mostrarimg,6000)
+
+  topics.forEach((topic, listaNum)=>{
+
+    topic.addEventListener('dblclick',()=>{
+    contaimg = listaNum;
+     mostrarimg()
+  })
+    topic.addEventListener('click',()=>{
+    contaimg = listaNum;
+   
+    mostrarimg()
+
+    
+  })
+  
+ })
+
+
 
 function puxando(){
   //controla a ida e volta das tres barras no responsivo
      if(conta > contador){
         
  tudo.classList.add('tudostyle')
-   ul.classList.add('ulstyle')
+  ul.classList.add('ulstyle')
    nav.classList.add('navstyle')
 
 
